31db2d71e9ad1754bac758a83d6e7964d81a96c1
Dark7lord <budda185@mail.ru>
2021-08-30 15:12:00 +0500

    Таблет версия "О Компании" готова

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -21,4 +21,29 @@
   for (let buttonCollapse of buttonsCollapse) {
     buttonCollapse.classList.add('footer__button-collapse--closed');
   }
+
+  // Обрезка символов в абзаце
+
+  const kitcut = (text, limit) => {
+    text = text.trim();
+    if (text.length <= limit) return text;
+    text = text.slice(0, limit);
+
+    return text.trim() + '..';
+  }
+
+  const aboutUsText = document.querySelector('.about-us p:last-of-type');
+  const oldText = aboutUsText.textContent;
+
+  if (document.documentElement.clientWidth < 1024) {
+    aboutUsText.textContent = kitcut(aboutUsText.textContent, 201)
+  }
+
+  window.addEventListener('resize', () => {
+    if (document.documentElement.clientWidth < 1024) {
+      aboutUsText.textContent = kitcut(aboutUsText.textContent, 201)
+    } else {
+      aboutUsText.textContent = oldText;
+    }
+  });
 })();
