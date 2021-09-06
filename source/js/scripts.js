9ff90fc4ff213d9da83f284598169d5ec44da1aa
Dark7lord <budda185@mail.ru>
2021-09-06 22:33:44 +0500

    Сделал маску для поля ввода телефона и открытие и закрытие модального окна

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (() => {
+  // aккардеон в футере
+
   let buttonsCollapse = document.querySelectorAll('.footer__button-collapse');
 
   for (let button of buttonsCollapse) {
@@ -48,4 +50,54 @@
       aboutUsText.textContent = oldText;
     }
   });
+
+  // Маска для поле ввода телефона (+JQuery) - господи как лаконично!
+
+  $('input[type="tel"]').mask("+7(999) 9999999");
+
+  // Oткрытие закрытие попапа заказа звонка в шапке
+
+  const buttonOpenModal = document.querySelector('.header__button-feedback');
+  const popupCallback = document.querySelector('.popup-callback');
+  const closePopupCallback = document.querySelector('.popup-callback__button-close');
+  const wrapperPopupCallback = document.querySelector('.popup-callback__wrapper');
+  const inputNamePopup = document.querySelector('.popup-callback input[type="text"]');
+
+
+  buttonOpenModal.addEventListener('click', () => {
+    popupCallback.classList.add('popup-callback--opened');
+    popupCallback.addEventListener('click', onOverlayClick);
+    window.addEventListener('keydown', onWindowPressEsc);
+    inputNamePopup.focus();
+  });
+
+  closePopupCallback.addEventListener('click', () => {
+    popupCallback.classList.remove('popup-callback--opened');
+    popupCallback.removeEventListener('click', onOverlayClick);
+    window.removeEventListener('keydown', onWindowPressEsc);
+  });
+
+  // // 5 простых строчек для реализации клика по оверлею, но сколько времени на них протрачено...
+  const onOverlayClick = (evt) => {
+    if (!wrapperPopupCallback.contains(evt.target)) {
+      popupCallback.classList.remove('popup-callback--opened');
+    }
+  };
+
+  const onWindowPressEsc = (evt) => {
+    if (evt.key === 'Escape') {
+      popupCallback.classList.remove('popup-callback--opened');
+    }
+  };
+
+  // попытки вспомнить localStorage
+  let isStorageSupport = true;
+
+  try {
+    let storageName = localStorage.getItem('name');
+    let storageEmail = localStorage.getItem('email');
+  } catch(error) {
+    isStorageSupport = false;
+    console.log(error);
+  }
 })();
